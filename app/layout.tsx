88a555d4fe9a4a3ef3ed2d93f45ac5a9e0afdd4f
Meirbek-dev <Meirbek.dev@gmail.com>
2024-10-06 17:29:35 +0500

    migrated to react 19rc

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 import "./globals.css";
+import React from "react";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -18,11 +19,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <body className={font.className}>
-        <ToastProvider />
-        <ModalProvider />
-        <Navbar />
-        {children}
-        <Footer />
+        <React.StrictMode>
+          <ToastProvider />
+          <ModalProvider />
+          <Navbar />
+          {children}
+          <Footer />
+        </React.StrictMode>
       </body>
     </html>
   );
